7f33696a0d6d2bff8bef2984ba83fc4dad299e9b
Jason Dove <jason@jasondove.me>
2021-01-03 11:14:49 -0600

    add tax opcode

--- semu/src/semu/CPU.scala
+++ semu/src/semu/CPU.scala
@@ -7,19 +7,23 @@ import scala.annotation.tailrec
 
 case class CPU(memory: Array[Int], start: Int) {
   private var _registerA: Int = 0
+  private var _registerX: Int = 0
   private var _status = CpuStatus.ValueSet.empty
   private var _programCounter: Int = start
 
   def run(): IO[Unit] = {
     @tailrec
     def loop(): IO[Unit] = {
-      memory(_programCounter) match {
+      val current = memory(_programCounter)
+      _programCounter += 1
+      current match {
         case 0xa9 =>
-          val param = memory(_programCounter + 1)
-          _registerA = param
-          _programCounter += 2
-          _status = if (registerA == 0) _status + CpuStatus.Zero else _status - CpuStatus.Zero
-          _status = if ((registerA & 0x80) != 0) _status + CpuStatus.Negative else _status - CpuStatus.Negative
+          val param = memory(_programCounter)
+          _programCounter += 1
+          lda(param)
+          loop()
+        case 0xaa =>
+          tax()
           loop()
         case 0x00 =>
           IO.unit
@@ -31,12 +35,31 @@ case class CPU(memory: Array[Int], start: Int) {
     loop()
   }
 
+  private def lda(value: Int): Unit = {
+    _registerA = value
+    updateZeroAndNegativeFlags(_registerA)
+  }
+
+  private def updateZeroAndNegativeFlags(result: Int): Unit = {
+    _status = if (result == 0) _status + CpuStatus.Zero else _status - CpuStatus.Zero
+    _status = if ((result & 0x80) != 0) _status + CpuStatus.Negative else _status - CpuStatus.Negative
+  }
+
+  private def tax(): Unit = {
+    _registerX = _registerA
+    updateZeroAndNegativeFlags(_registerX)
+  }
+
   def registerA: Int = _registerA
+
+  def registerX: Int = _registerX
+
   def status: CpuStatus.ValueSet = _status
+
   def programCounter: Int = _programCounter
 
   override def toString: String =
-    s"a: ${_registerA}, s: ${_status}, pc: 0x${_programCounter.toHexString}"
+    s"a: ${_registerA}, x: ${_registerX} s: ${_status}, pc: 0x${_programCounter.toHexString}"
 }
 
 object CPU {
--- semu/test/src/semu/CpuSuite.scala
+++ semu/test/src/semu/CpuSuite.scala
@@ -23,4 +23,23 @@ object CpuSuite extends SimpleIOSuite {
       _ <- expect(cpu.status.contains(CpuStatus.Negative)).failFast
     } yield success
   }
-}
+
+  simpleTest("TAX should set zero flag") {
+    val program = Array(0xa9, 0x00, 0xaa, 0x00)
+    val cpu = CPU.load(program)
+    for {
+      _ <- cpu.run()
+      _ <- expect(cpu.registerX == 0).failFast
+      _ <- expect(cpu.status.contains(CpuStatus.Zero)).failFast
+    } yield success
+  }
+
+  simpleTest("TAX should set negative flag") {
+    val program = Array(0xa9, 0xff, 0xaa, 0x00)
+    val cpu = CPU.load(program)
+    for {
+      _ <- cpu.run()
+      _ <- expect(cpu.registerX == 0xff).failFast
+      _ <- expect(cpu.status.contains(CpuStatus.Negative)).failFast
+    } yield success
+  }}
